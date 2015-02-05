85e1876f6317b55bb659d46021795b4219e5ce99
eunice <eunicekokor@users.noreply.github.com>
2015-02-04 21:55:05 -0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  console.log("github version 4 ready.");
+  console.log("github version 5 ready.");
   
   $('#transition').fadeIn(1000, function(){
     $('#logo').animate({'margin-top': '5%'}, 800, function(){
