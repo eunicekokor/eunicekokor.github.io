74e6e2739c8d0b2b3dc8d32bc622dcb7baf42222
eunice <eunicekokor@users.noreply.github.com>
2015-02-14 11:13:54 -0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
                  });
               });
               
-              $('.social').draggable();
+              $('#logo').draggable();
             });
           });
         });
