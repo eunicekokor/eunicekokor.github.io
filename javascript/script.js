0cda4a99cddd73e42e53338ced2bcdb44572bc69
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-05-07 15:55:04 -0400

    cleaned up site

--- javascript/script.js
+++ javascript/script.js
@@ -1,35 +1,20 @@
 $(document).ready(function(){
   console.log("github version ready.");
   
-  $('#transition').fadeIn(1000, function(){
-    $('#logo').animate({'margin-top': '5%'}, 800, function(){
-      $('#first').fadeIn(800, function(){ 
-        $('#twit').animate({'margin-top': '0'}, 500, function(){
-          $('#linked').animate({'margin-top': '0px'}, 750);
-          $('#git').animate({'margin-top': '0px'}, 1500, function(){
-            $('#mail').animate({'margin-top': '0px'}, 500, function(){
-              $('#resume').animate({'margin-top': '0px'}, 750);
-              $('#moving').fadeIn(750);
-              
-              $('#first').hover(function(){
-                $('#first').fadeOut(1000, function(){
-                  $('#second').fadeIn(500);
-                 });
-              });
-              $('#second').hover(function(){
-                $('#second').fadeOut(1000, function(){
-                  $('#first').fadeIn(500);
-                 });
-              });
-              
-            });
-          });
-        });
-      });
+  $('#transition').fadeIn(1000, function(){       
+    $('#first').hover(function(){
+      $('#first').fadeOut(1000, function(){
+        $('#second').fadeIn(500);
+       });
+    });
+    $('#second').hover(function(){
+      $('#second').fadeOut(1000, function(){
+        $('#first').fadeIn(500);
+       });
     });
   });
   
-$(function(){
+/*$(function(){
       $('#moving').typed({
         strings: ["^2500 <b>Engineering</b> Student who studies Computer Science <br> ^2500 & Sustainable Engineering & Entrepreneurship </b> @Columbia ^2000", "STEM educator ^1000 & <br> STEM education advocate ^2000", "technologist ^1000 who <b>builds</b> <br> for ^500 <b>sustainability</b> ^2000", "making a <b>difference</b> ^2000 through technology"],
         typeSpeed: 0,
@@ -38,7 +23,7 @@ $(function(){
       });
   });
 
-/*var adjectives = ['Computer Science Student ^1000 @Columbia Engineering', 'STEM educator ^1000 & education advocate', 'technologist ^1000 who builds for sustainability', 'making a difference ^2000 through technology'];
+var adjectives = ['Computer Science Student ^1000 @Columbia Engineering', 'STEM educator ^1000 & education advocate', 'technologist ^1000 who builds for sustainability', 'making a difference ^2000 through technology'];
 
 var para = $('#moving');
 var delayInMillis = 2500;
