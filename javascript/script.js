355bcd5e05b56f37d852ab754756b744573a8965
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-05-15 10:27:17 -0700

    refresh button

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,3 @@
-$(function(){
+document.ready(function(){
   console.log("github version ready.");
-    $('a[href*="#"]').click(function(e) {
-    e.preventDefault();
-    var $target = $($(this).attr('href'));
-    var scrollTop = $target.offset().top;
-    $('html, body').animate({'scrollTop': scrollTop}, 500);
-  });
 });
