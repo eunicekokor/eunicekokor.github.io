ac5b295ba98333a4be9207310dd900b3a17eb912
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-10-28 00:50:28 -0400

    some finishing things

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
   $("#mylogo").animate({"margin-top":"12rem"}, 700);
   $("#mylogo").animate({"margin-top":"3rem"}, 500);
   $("#mylogo").animate({"margin-top":"9rem"}, 500);
-  $("#mylogo").animate({"margin-top":"7rem"}, 250), function(){
+  $("#mylogo").animate({"margin-top":"7rem"}, 250, function(){
       $(".intro_text").display();
-  }
+  });
 
   $("#mylogo").click(function(){
     this.fadeOut();
@@ -14,6 +14,14 @@ $(document).ready(function(){
   $( ".page_heading" ).click(function() {
   $( ".subheading" ).toggle( "explode" );
 });
+
+  // $('.project-feature').click(function(){
+  //   var src = $(this).attr("src");
+  //   console.log(src);
+  //   $('.overlay').css('background-image','url(' + src +')');
+  //   $('overlay').show();
+  //   console.log('got here');
+  // })
 
 
 });
