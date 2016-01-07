94c7acd2f7fd042946deab26e5d3256562f5444e
Eunice Emefa Kokor <eek2138@columbia.edu>
2016-01-07 01:03:59 -0800

    mvp for simple

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ import requests, json
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-#section title
-#subheading
-#paragraph stuff
 website_info = None
-projects = None
 
 @app.route("/")
 def home():
@@ -19,42 +15,10 @@ def home():
 def get_website_info():
   global website_info
   if not website_info:
-    with open('data/website_info.json') as f:
+    with open('data/projects.json') as f:
       website_info = json.loads(f.read())
   return website_info
 
-@app.route("/home")
-def index():
-  website_info = get_website_info()
-  website_info['current'] = 'home'
-  return render_template("index.html", info=website_info)
-
-@app.route("/about")
-def about():
-  website_info = get_website_info()
-  website_info['current'] = 'about'
-  return render_template("about.html", info=website_info)
-
-@app.route("/projects")
-def build():
-  # TODO: change the way project information sourced
-  website_info = get_website_info()
-  website_info['current'] = 'projects'
-  projects = get_projects()
-  return render_template("projects.html", info=website_info, projects=projects)
-
-def get_projects():
-  global projects
-  if not projects:
-    with open('data/projects.json') as f:
-      projects = json.loads(f.read())
-  return projects
-
-@app.route("/extras")
-def heart():
-  website_info = get_website_info()
-  website_info['current'] = 'extras'
-  return render_template("extras.html", info=website_info)
 
 @app.errorhandler(404)
 def page_not_found(error):
--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,56 +1,5 @@
 $(document).ready(function(){
-  var delay = 4500;
-  $("#mylogo").click(function(){
-    $("#mylogo").css('animation','logo-rotate-mobile 4s ease-in-out 1');
-    setTimeout(function(){ window.location = '/about'; }, delay);
-  });
 
-  $(".intro_text").click(function(){
-    $("#mylogo").css('animation','logo-rotate-mobile 4s ease-in-out 1');
-    setTimeout(function(){ window.location = '/about'; }, delay);
-  });
-
-
-  $(".organization_list").click(function(){
-    var class_detail = "";
-    var important_class = "";
-    var class_detail = $(this).attr("class");
-    class_detail = class_detail.split(' ');
-    var important_class = class_detail[1];
-    $("." + important_class+".organization_detail").toggle();
-  });
-
-  $(".organization_group").click(function(){
-    var class_detail = "";
-    var important_class = "";
-    var class_detail = $(this).attr("class");
-    class_detail = class_detail.split(' ');
-    var important_class = class_detail[1];
-    $("." + important_class+".organization_detail").toggle();
-  });
-  $(".organization_detail").click(function(){
-    $(this).toggle();
-  });
-
-  $(".poem_detail").click(function(){
-    $(this).toggle();
-  });
-
-  $(".fa-bars").click(function(){
-    $(this).css('animation-iteration-count',1);
-  })
-
-//   $( ".page_heading" ).click(function() {
-//   $( ".subheading" ).toggle( "explode" );
-// });
-
-  // $('.project-feature').click(function(){
-  //   var src = $(this).attr("src");
-  //   console.log(src);
-  //   $('.overlay').css('background-image','url(' + src +')');
-  //   $('overlay').show();
-  //   console.log('got here');
-  // })
 
 
 });
