643515dd587fa10db600ac22720b719681212a4a
Eunice Emefa Kokor <eek2138@columbia.edu>
2016-01-26 13:12:40 -0500

    setup for color changes

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -3,4 +3,23 @@ $(document).ready(function(){
         e.preventDefault();
         $(this).toggleClass('hover_effect');
     });
-})
+
+
+function funStuff(){
+    console.log("y1y");
+    var colors=["peach", "rust", "orange", "ruddy", "seagreen"] ;
+
+    for (var i = 0; i < colors.length; i++) {
+      if ($('.showcase.col-1-4').hasClass(colors[i])) {
+        $(this).removeClass(colors[i]);
+        $(this).addClass(colors[Math.floor(Math.random()*colors.length)]);
+      }
+    }
+
+    setTimeout(funStuff(), 5000);
+
+  };
+
+funStuff();
+
+});
